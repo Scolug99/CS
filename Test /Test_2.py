d6d06eb71407a5ed69ea6115fc653c1e5067fa08
Scolug99 <j-l@bluewin.ch>
2025-11-11 13:54:53 +0100

    kei bock

--- Test /Test_2.py
+++ Test /Test_2.py
@@ -5,15 +5,15 @@ original_list = [i for i in range (10)]
 
 even_list = return_even(original_list)
 
-new_list = [i for i in range (10)]
-
-odd_list = return_odd(new_list)
+odd_list = return_odd(original_list)
 
 st.write('hooray, we made it')
 
 st.write('Hello, world')
 
 st.write(even_list)
+
+st.write(odd_list)
 
 def addition(a,b):
     return a + b
