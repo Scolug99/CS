46d3f324eda4b9fc6ea8c84712ffd6351f10f103
Scolug99 <j-l@bluewin.ch>
2025-11-11 08:56:33 +0100

    test now

--- Test /Test_2.py
+++ Test /Test_2.py
@@ -1,12 +1,12 @@
 import streamlit as st
 #from Feature_01 import return_even
 
-original_list = [i for i in range (10)]
+#original_list = [i for i in range (10)]
 
-even_list = return_even(original_list)
+#even_list = return_even(original_list)
 
 st.write('hooray, we made it')
 
 st.write('Hello, world')
 
-st.write(even_list)
+#st.write(even_list)
